dcd98d2bdc2dca52f45138c18dc4ba40caf1eb71
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-09-20 13:13:00 -0400

    Add General Information boilerplate

---
+++ src/General.js
@@ -0,0 +1,20 @@
+import { Component } from "react";
+import './styles/General.css';
+
+class General extends Component {
+    render() {
+        const name = "John Doe";
+        const email = "johndoe@deer.com";
+        const phone = "(123) 123-1234";
+
+        return (
+            <div className="general">
+                <input className="edit large" type="text" id="name" value={name} />
+                <input className="edit" type="text" id="email" value={email} />
+                <input className="edit" type="text" id="phone" value={phone} />
+            </div>
+        );
+    }
+}
+
+export default General;

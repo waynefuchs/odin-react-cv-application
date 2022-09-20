e7a17c79db151e486b0c5fb23b05e7d10f17ba2e
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-09-20 15:48:43 -0400

    Add Education

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Header from './Header';
 import General from './General';
 import Footer from './Footer';
 import './styles/App.css';
+import Education from './Education';
 
 function App() {
   return (
     <div>
       <Header />
       <General />
+      <Education />
       <Footer />
     </div>
   );

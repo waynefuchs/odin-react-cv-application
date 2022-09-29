fd2c0055b10d0dd3caba62998b8a2353e6558a0c
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-09-29 17:06:00 -0400

    Add state variable for counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,22 @@
 const { Component } = require("react");
 
 class Counter extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      counter: 0
+    }
+
+
+    console.log("Constructor");
+  }
+
   render() {
     return (
       <div>
         <div clasName="counter">
-          Counter {0}
+          Counter {this.state.counter}
         </div>
       </div>
     );
